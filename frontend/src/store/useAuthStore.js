5118cf471fba361f096af79afbc8cb71b6c4198d
pankajkrbansal <pankajbansal.pkb@gmail.com>
2024-11-23 23:49:59 +0530

    Added Login

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -12,26 +12,48 @@ export const useAuthStore = create((set) => ({
   checkAuth: async () => {
     try {
       const res = await axiosInstance.get("/auth/check");
-      set({authUser:res.data})
+      set({ authUser: res.data });
     } catch (err) {
-        console.log(`Error In checkAuth ${err}`)
-      set({authUser:null})
-    }finally{
-        set({isCheckingAuth:false})
+      console.log(`Error In checkAuth ${err}`);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
     }
   },
-  signup: async(data) => {
-    set({isSigningUp:true})
-    try{
-      const res = await axiosInstance.post("/auth/signup", data)
-      set({authUser:res.data})
-      toast.success("Account Created Successfully")
-      
-    }catch(err){
-      toast.error(err.response.data.message)
-    }finally{
-    set({isSigningUp:false})
-
+  signup: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/signup", data);
+      set({ authUser: res.data });
+      toast.success("Account Created Successfully");
+    } catch (err) {
+      toast.error(err.response.data.message);
+    } finally {
+      set({ isSigningUp: false });
     }
-  }
+  },
+  login: async (data) => {
+    set({ isLoggingIn: true });
+    try {
+      const res = await axiosInstance.post("/auth/login", data);
+      set({ authUser: res.data });
+      toast.success("Logged in successfully");
+
+      // get().connectSocket();
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully");
+      // get().disconnectSocket();
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  },
 }));
